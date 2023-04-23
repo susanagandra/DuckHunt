88c18960cd1195a8489f4f8ea6eb115de9221ec7
susanagandra <122367270+susanagandra@users.noreply.github.com>
2023-04-23 09:29:50 +0100

    clean version

---
+++ README.md
@@ -0,0 +1,22 @@
+Duck Hunt - Game
+
+Welcome to Duck Hunt, a classic shooting game brought to life!
+
+Description
+Duck Hunt is a simple shooting game where the player tries to shoot down flying ducks as they move across the screen. The game features a score system where points are earned for hitting ducks, and points are deducted for missed shots. The player has a limited number of bullets and must reload after each shot.
+
+Tools used to develop this project
+
+In order to develop this project we used: JavaScript, CSS and HTML.
+
+HTML: used to structure the webpage and create the various elements, such as the game container and the dog and duck divs.
+
+CSS: used to style the webpage and add visual effects to the game elements, such as the background image and the animation for the laughing dog.
+
+JavaScript: used to create the game logic and interactivity. It was used to create the ducks and the dog, control their movement and animation, and detect user input (such as mouse clicks and key presses) to shoot the ducks.
+
+
+This game was created by Daniela S and Susana Gandra for Mindera Code Academy.
+
+Credits:
+Credits to Nintendo for the original game "Duck Hunt".
--- scripts/game.js
+++ scripts/game.js
@@ -101,7 +101,8 @@ const createDuck = () => {
           //kill duck, calculate shots and score
           duck.addEventListener('click', (event) => {
             event.target.classList.add("shot");
-           
+            const audioShot = new Audio('/sounds/gunSound.mp3');
+            audioShot.play();
           
             setTimeout(() => {
               duck.parentNode.removeChild(duck);
@@ -117,135 +118,117 @@ const createDuck = () => {
           return duck;
 };
 
+const game = document.getElementById("game");
 
-    const game = document.getElementById("game");
+  game.addEventListener("click", (event) => {
+    countShots();
+    checkWinner();
+  });
 
-        game.addEventListener("click", (event) => {
-          countShots();
-          checkWinner();
-        
-        } )
+const countShots = () => {
+    shots -= 1
+    document.getElementById("bullet"+(3-shots)).style.display = "none";
+};
 
-    
-    function countShots(){
-      console.log("shots: " + shots)
-        shots -= 1
-        console.log("shots: " + shots)
-        document.getElementById("bullet"+(3-shots)).style.display = "none"
-    }
+const reseatShots = () => {
+    shots = 0;
+    document.getElementById("bullet1").style.display = "inline";
+    document.getElementById("bullet2").style.display = "inline";
+    document.getElementById("bullet3").style.display = "inline";
+};
 
-    function reseatShots(){
-        shots = 0;
-        document.getElementById("bullet1").style.display = "inline";
-        document.getElementById("bullet2").style.display = "inline";
-        document.getElementById("bullet3").style.display = "inline";
-    }
+const dogwithduck = () => {
+  const imagemcontainer = document.getElementById("image-container");
+  const dogwithduck = document.createElement('div');
 
-    function dogwithduck(){
-      const imagemcontainer = document.getElementById("image-container")
-      const dogwithduck = document.createElement('div');
-      dogwithduck.setAttribute("id", "dogshot")
-      imagemcontainer.appendChild(dogwithduck)
-      setTimeout(function() {
+  dogwithduck.setAttribute("id", "dogshot");
+  imagemcontainer.appendChild(dogwithduck);
+
+    setTimeout(function() {
         const divParaRemover = document.getElementById("dogshot");
         divParaRemover.parentNode.removeChild(divParaRemover);
       }, 5000);
-    }
+};
 
-    function countDeadDucks(){
-        let duckTodead = document.getElementById("liveDuck"+deadDucks)
-        duckTodead.src = "images/duckDead.png"
-        deadDucks += 1;
-    }
+const countDeadDucks = () => {
+  let duckTodead = document.getElementById("liveDuck"+deadDucks);
+    duckTodead.src = "images/duckDead.png";
+    deadDucks += 1;
+};
 
-    function reseatDeadDucks(){
-        deadDucks = 0
-        let liveDuck0 = document.getElementById("liveDuck0")
-        let liveDuck1 = document.getElementById("liveDuck1")
-        let liveDuck2 = document.getElementById("liveDuck2")
+const reseatDeadDucks = () =>{
+  deadDucks = 0;
+  let liveDuck0 = document.getElementById("liveDuck0");
+  let liveDuck1 = document.getElementById("liveDuck1");
+  let liveDuck2 = document.getElementById("liveDuck2");
 
-        liveDuck0.remove();
-        liveDuck1.remove();
-        liveDuck2.remove();
+  liveDuck0.remove();
+  liveDuck1.remove();
+  liveDuck2.remove();
+};
 
-
-    }
-
+const addScore = () => {
     let score = document.getElementById("score");
-    function addScore(){
-        scoreValue += 100;
-        score.textContent = scoreValue;
-    }
+    scoreValue += 100;
+    score.textContent = scoreValue;
+};
   
+const resetScore = () => {
     let score2 = document.getElementById("score");
-    function resetScore(){
-      scoreValue = 0;
-      score2.textContent = scoreValue;
-  }
+    scoreValue = 0;
+    score2.textContent = scoreValue;
+};
     
-     function checkWinner() {
-           
-        const ducks = document.querySelectorAll(".duck");
+const checkWinner = () => {
+         
+  const ducks = document.querySelectorAll(".duck");
 
+  if (ducks.length != 0 && shots === 0) {
+      const box = document.getElementById("game");
+      const htmlBlock = `<div id=overlay2> <div> <div>Game over!!</div> <div> Try again </div></div></div>`;
+      game.innerHTML += htmlBlock;  
 
-        if(ducks.length != 0 && shots === 0){
-          console.log("shots: " + shots);
+      setTimeout(function() {
+        const overlay2 = document.getElementById("overlay2"); 
+        overlay2.style.display = "none";           
+        playAgainButton.style.display = "flex";
+        const ducks = document.querySelectorAll('.duck');
 
-          
-          const box = document.getElementById("game")
-          const htmlBlock = `<div id=overlay2> <div> <div>Game over!!</div> <div> Try again </div></div></div>`;
-          game.innerHTML += htmlBlock;  
-
-          setTimeout(function() {
-            const overlay2 = document.getElementById("overlay2"); 
-            overlay2.style.display = "none";           
-            playAgainButton.style.display = "flex";
-            const ducks = document.querySelectorAll('.duck');
-
-            ducks.forEach(duck => {
-            duck.remove();
-            });
-            resetScore();
-            reseatDeadDucks();  
-            reseatShots();
-           
-            
-          }, 6000);
-
-        }
+        ducks.forEach(duck => {
+            duck.remove();});
+        
+        resetScore();
+        reseatDeadDucks();  
+        reseatShots();
+        }, 6000);
+      };
     
-        if (ducks.length === 0) {
-
-          const box = document.getElementById("game")
-          const htmlBlock = `<div id=overlay2> <div> <div>Congratulation!!</div> <div> You win the game </div></div></div>`;
-          game.innerHTML += htmlBlock;          
-
-          
-            setTimeout(function() {
-              const overlay2 = document.getElementById("overlay2");
-              overlay2.style.display = "none";
-              playAgainButton.style.display = "flex";
-              resetScore();
-              reseatDeadDucks();  
-              reseatShots();
-            
-              
-            }, 6000);
-        }
-      }
+  if (ducks.length === 0) {
+      const box = document.getElementById("game");
+      const htmlBlock = `<div id=overlay2> <div> <div>Congratulation!!</div> <div> You win the game </div></div></div>`;
+      game.innerHTML += htmlBlock;          
+ 
+      setTimeout(function() {
+        const overlay2 = document.getElementById("overlay2");
+        overlay2.style.display = "none";
+        playAgainButton.style.display = "flex";
+        resetScore();
+        reseatDeadDucks();  
+        reseatShots();      
+        }, 6000);
+      };
+};
     
-    function createLiveDucks(){
-      const liveduck = document.getElementById("liveduck");
+const createLiveDucks = () => {
+  const liveduck = document.getElementById("liveduck");
       for (let i = 0; i < numberofducks; i++) {
         const duckImg = document.createElement("img");
         duckImg.id = "liveDuck" + i;
         duckImg.src = "images/duckLive.png";
         duckImg.alt = "duck lives";
         document.body.appendChild(duckImg);
-      }
-
-
-    }
+      };
+};
 
 const dogMove = () => {
   const dog = document.createElement('div');
@@ -259,10 +242,12 @@ const dogMove = () => {
   let position = 0;
     
   const audioBark = new Audio('/sounds/barkDucks.mp3');
+  const audioSniff = new Audio('/sounds/sniff.mp3');
+  audioSniff.play();
   audioBark.play();
     
-    setInterval(() => {
-      spriteSheet.style.backgroundPosition = `-${position}px 120px`;
+    const interval = setInterval(() => {
+        spriteSheet.style.backgroundPosition = `-${position}px 120px`;
     
         if (position < widthOfSpriteSheet) {
           position = position + widthOfEachSprite;
@@ -272,10 +257,14 @@ const dogMove = () => {
           spriteSheet.remove();
           audioBark.pause();
           audioBark.currentTime = 0;
-            audioBark.onended = () => {
+          audioSniff.currentTime = 0;
+          audioBark.onended = () => {
             audioBark.remove();
-            };
-        };
+          };
+          audioSniff.onended = () => {
+            audioSniff.remove();
+          };
+        }
       }, 800);
 };
     
